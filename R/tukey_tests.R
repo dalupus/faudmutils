5a30e36205ded1df26908ca8a56c726740988bd5
Michael Crawford <dalupus@gmail.com>
2015-10-03 16:05:27 -0400

    Add tukey test stuff

---
+++ R/tukey_tests.R
@@ -0,0 +1,31 @@
+
+
+
+#' Function to automagically create tukey test tables from an anova anlysis
+#'
+#' @param aov Anova analysis of from \code{\link[stats]{aov}}
+#' @param which This is what you factors you want to test and can either be a single string or array of strings c('F1','F2')
+#' @param metric The metric you want to use for evaluation
+#' @param type Type of output you would like.  valid values are 'html' and 'latex'
+#' @param caption String containing the caption of the table
+#' @param label Latex label to give the table
+#' @param digits Number of significant digits to include in the table
+#' @param file The file in which to output the results ("" means it simply prints to the screen)
+HSDTable <- function(aov, which=NULL, metric="AUC",type="html",cap="Tukey HSD Test",label="",digits=3,file=""){
+  if(is.null(which)){
+    which = ls(df.aov$xlevels)
+  }
+  hsd <- HSD.test(aov,which)
+  hsd.groups <- hsd$groups
+  hsd.means <- hsd$means
+  hsd.means$trt<-trimws(as.character(rownames(hsd.means)))
+  hsd.groups$trt <- trimws(as.character(hsd.groups$trt))
+  hsd <- left_join(hsd.groups,hsd.means,by=c('trt'))
+  hsd <- select(hsd,trt,M,means,std)
+  if(length(which) > 1) {
+    colnames(hsd) <- c(paste(which, collapse = ':'), 'Group', metric, 'stdev')
+  } else {
+    colnames(hsd) <- c(which, 'Group', metric, 'stdev')
+  }
+  print(xtable(hsd,caption=cap,label=label,digits=digits),type=type,file)
+}
