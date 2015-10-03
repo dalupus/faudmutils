e076148b710c0ff61f801e42afe69650014f40ed
Michael Crawford <dalupus@gmail.com>
2015-10-03 17:05:17 -0400

    add vignettes

---
+++ inst/doc/sample_analysis.R
@@ -0,0 +1,48 @@
+## ------------------------------------------------------------------------
+library(knitr)
+library(faudmutils)
+library(RWeka)
+suppressPackageStartupMessages(library(dplyr))
+library(xtable)
+library(agricolae)
+
+## ------------------------------------------------------------------------
+zipfile <- system.file("extdata","newreview-wordvector-output.zip",package="faudmutils")
+
+## ------------------------------------------------------------------------
+df <- mapKeys(zipfile)
+
+## ------------------------------------------------------------------------
+unique(df$Learner)  # Learner names are now human readible
+unique(df$Dataset) #  No more dataset numbers
+
+## ------------------------------------------------------------------------
+df$Dataset <- plyr::revalue(df$Dataset,c("reviewSpamData-string-hotel.arff"="hotel",
+                                           "reviewSpamData-string-restaurant.arff"="restraunt",
+                                           "reviewSpamData-string-doctor.arff"="doctor"))
+table(df$Dataset)
+
+## ------------------------------------------------------------------------
+sampledDF <- sample_n(df,10)
+sampledDF <- select(sampledDF,Dataset,SamplingTechnique,WordsToKeep,Learner,AUC)
+kable(sampledDF,row.names = FALSE)
+
+## ------------------------------------------------------------------------
+df.aov <- aov(AUC ~ Dataset*Learner,df)
+summary(df.aov)
+
+## ----results='asis'------------------------------------------------------
+print(xtable(df.aov),type="html")
+
+## ------------------------------------------------------------------------
+print(xtable(df.aov),type="latex")
+
+## ------------------------------------------------------------------------
+HSD.test(df.aov,"Learner",console = TRUE)
+
+## ---- results='asis'-----------------------------------------------------
+HSDTable(df.aov)
+
+## ---- results='asis'-----------------------------------------------------
+HSDTable(df.aov,which="Learner")
+
